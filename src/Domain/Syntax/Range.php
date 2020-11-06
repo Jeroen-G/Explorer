67ab6adb11a88a06bd14abbe637b14aff9c36267
Jeroen <jgroenendijk@enrise.com>
2020-11-06 16:15:19 +0100

    fix boost error in range - fixes #15

--- src/Domain/Syntax/Range.php
+++ src/Domain/Syntax/Range.php
@@ -27,8 +27,7 @@ class Range implements SyntaxInterface
     public function build(): array
     {
         return ['range' => [
-            $this->field => $this->definitions,
-            'boost' => $this->boost,
+            $this->field => array_merge( $this->definitions, ['boost' => $this->boost]),
         ]];
     }
 
