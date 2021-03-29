67b5e68d230915056843c4a9070e50d5dc8d7aa2
Vincent Hagen <vinnie@script4web.nl>
2021-03-29 19:47:37 +0200

    Add rescoring to query

---
+++ src/Domain/Query/Rescoring.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeroenG\Explorer\Domain\Query;
+
+use JeroenG\Explorer\Domain\Syntax\SyntaxInterface;
+
+class Rescoring implements SyntaxInterface
+{
+    public const SCORE_MODE_TOTAL = 'total';
+
+    public const SCORE_MODE_MULTIPLY = 'multiply';
+
+    public const SCORE_MODE_AVERAGE = 'avg';
+
+    public const SCORE_MODE_MAX = 'max';
+
+    public const SCORE_MODE_MIN = 'min';
+
+    private int $windowSize = 10;
+
+    private string $scoreMode = Rescoring::SCORE_MODE_TOTAL;
+
+    private float $queryWeight = 1;
+
+    private float $rescoreQueryWeight = 1;
+
+    private SyntaxInterface $query;
+
+    public function build(): array
+    {
+        return [
+            'window_size' => $this->windowSize,
+            'query_weight' => $this->queryWeight,
+            'rescore_query_weight' => $this->rescoreQueryWeight,
+            'query' => [
+                'score_mode' => $this->scoreMode,
+                'rescore_query' => $this->query->build()
+            ],
+        ];
+    }
+
+    public function setWindowSize(int $windowSize): void
+    {
+        $this->windowSize = $windowSize;
+    }
+
+    public function setScoreMode(string $scoreMode): void
+    {
+        $this->scoreMode = $scoreMode;
+    }
+
+    public function setQueryWeight($queryWeight): void
+    {
+        $this->queryWeight = $queryWeight;
+    }
+
+    public function setRescoreQueryWeight($rescoreQueryWeight): void
+    {
+        $this->rescoreQueryWeight = $rescoreQueryWeight;
+    }
+
+    public function setQuery(SyntaxInterface $query): void
+    {
+        $this->query = $query;
+    }
+}
