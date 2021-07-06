6ebaf8941e499f630bdaeb63a6a74d52a6ed1641
Jeroen <jgroenendijk@enrise.com>
2021-07-06 10:08:34 +0200

    Deprecate commands duplicated by scout in v9

--- changelog.md
+++ changelog.md
@@ -10,6 +10,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - The new match_phrase query (#73)
 - Ability to optionally set more parameters for a few queries (#68)
 
+### Deprecated
+- The `elastic:create` and `elastic:delete` are deprecated in favour of `scout:index` and `scout:delete-index`
+
 ## [2.4.1]
 
 ### Fixed
--- src/Infrastructure/Console/ElasticDelete.php
+++ src/Infrastructure/Console/ElasticDelete.php
@@ -19,6 +19,8 @@ class ElasticDelete extends Command
 
     public function handle(ElasticIndexAdapter $adapter, IndexConfigurationRepositoryInterface $indexConfigurationRepository): int
     {
+        $this->warn('This command is deprecated and will be removed in 3.0. Use scout:delete-index `name` instead.');
+
         $config = config('explorer');
 
         if (!$config) {
