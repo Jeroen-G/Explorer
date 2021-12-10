14ff5b00df009ce61d1a23508e2ba3fd62b9fa66
Oğuz Can Sertel1 <oguzcansertel@gmail.com>
2021-12-11 00:27:44 +0300

    change

--- src/Infrastructure/Scout/ElasticEngine.php
+++ src/Infrastructure/Scout/ElasticEngine.php
@@ -153,11 +153,11 @@ class ElasticEngine extends Engine
 
         $objectIds = $this->mapIds($results)->all();
         $objectIdPositions = array_flip($objectIds);
-        //  just added comment to be sure the action error is not about this.
+        //  change cursor to lazy
         return $model->getScoutModelsByIds(
             $builder,
             $objectIds
-        )->cursor()->filter(function ($model) use ($objectIds) {
+        )->lazy()->filter(function ($model) use ($objectIds) {
             return in_array($model->getScoutKey(), $objectIds, false);
         })->sortBy(function ($model) use ($objectIdPositions) {
             return $objectIdPositions[$model->getScoutKey()];
