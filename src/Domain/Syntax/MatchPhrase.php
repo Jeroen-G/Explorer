8c6b4426ca61b7de582f4409b7e3f81ab66b3817
cbomprezzi <cbomprezzi@it.iliad.com>
2021-12-29 17:07:41 +0100

    add matchphrase

---
+++ src/Domain/Syntax/MatchPhrase.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeroenG\Explorer\Domain\Syntax;
+
+class MatchPhrase implements SyntaxInterface
+{
+    private string $field;
+
+    /** @var mixed */
+    private $value;
+
+    public function __construct(string $field, $value = null)
+    {
+        $this->field = $field;
+        $this->value = $value;
+    }
+
+    public function build(): array
+    {
+        $query = [ 'query' => $this->value ];
+
+        return ['match_phrase' => [ $this->field => $query ] ];
+    }
+}
