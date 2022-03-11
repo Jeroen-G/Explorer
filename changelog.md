c1fbc26740b12d671ace03d01db42d9931e0aeb9
Jeroen <jgroenendijk@enrise.com>
2022-03-11 12:17:50 +0100

    Add nested and max aggregations

--- changelog.md
+++ changelog.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Added
 - Laravel Scout's prefix is added to the index name if present
+- Max and Nested aggregations
 
 ## [3.0.0]
 
--- tests/Unit/Aggregations/TermsAggregationTest.php
+++ tests/Unit/Aggregations/TermsAggregationTest.php
@@ -7,7 +7,7 @@ namespace JeroenG\Explorer\Tests\Unit\Aggregations;
 use JeroenG\Explorer\Domain\Aggregations\TermsAggregation;
 use PHPUnit\Framework\TestCase;
 
-class AggregationTermsTest extends TestCase
+class TermsAggregationTest extends TestCase
 {
     public function test_it_builds(): void
     {
