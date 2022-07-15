5300e8379021256e5b2ff68c06bce0c5bd91c71b
Vincent Hagen <vinnie@script4web.nl>
2022-07-15 21:02:09 +0200

    Add a connection builder with tests and docs for all the config options

---
+++ src/Infrastructure/Elastic/ElasticClientBuilder.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JeroenG\Explorer\Infrastructure\Elastic;
+
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
+use Illuminate\Contracts\Config\Repository;
+
+final class ElasticClientBuilder
+{
+    private const HOST_KEYS = ['host', 'port', 'scheme'];
+
+    public static function fromConfig(Repository $config): ClientBuilder
+    {
+        $builder = ClientBuilder::create();
+
+
+        $hostConnectionProperties = array_filter(
+            $config->get('explorer.connection'),
+            static fn ($key) => in_array($key, self::HOST_KEYS, true),
+            ARRAY_FILTER_USE_KEY
+        );
+
+        $builder->setHosts([$hostConnectionProperties]);
+
+        if ($config->has('explorer.additionalConnections')) {
+            $builder->setHosts([$config->get('explorer.connection'), ...$config->get('explorer.additionalConnections')]);
+        }
+        if ($config->has('explorer.connection.selector')) {
+            $builder->setSelector($config->get('explorer.connection.selector'));
+        }
+
+        if($config->has('explorer.connection.api')) {
+            $builder->setApiKey(
+                $config->get('explorer.connection.api.id'),
+                $config->get('explorer.connection.api.key')
+            );
+        }
+
+        if($config->has('explorer.connection.elasticCloudId')) {
+            $builder->setElasticCloudId(
+                $config->get('explorer.connection.elasticCloudId'),
+            );
+        }
+
+        if($config->has('explorer.connection.auth')) {
+            $builder->setBasicAuthentication(
+                $config->get('explorer.connection.auth.username'),
+                $config->get('explorer.connection.auth.password')
+            );
+        }
+
+        if($config->has('explorer.connection.ssl.verify')) {
+            $builder->setSSLVerification($config->get('explorer.connection.ssl.verify'));
+        }
+
+        if($config->has('explorer.connection.ssl.key')) {
+            [$path, $password] = self::getPathAndPassword($config->get('explorer.connection.ssl.key'));
+            $builder->setSSLKey($path, $password);
+        }
+
+        if($config->has('explorer.connection.ssl.cert')) {
+            [$path, $password] = self::getPathAndPassword($config->get('explorer.connection.ssl.cert'));
+            $builder->setSSLCert($path, $password);
+        }
+
+        return $builder;
+    }
+
+    /**
+     * @param array|string $config
+     */
+    private static function getPathAndPassword(mixed $config): array
+    {
+        return is_array($config) ? $config : [$config, null];
+    }
+}
