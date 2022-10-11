15656a0893ac1c6497a192d0214cc6df3ae4f799
David <david@nyehandel.se>
2022-10-11 23:57:35 +0200

    Test new parameters for wildcard

--- tests/Unit/Syntax/WildcardTest.php
+++ tests/Unit/Syntax/WildcardTest.php
@@ -18,7 +18,7 @@ class WildcardTest extends TestCase
         $wildcard = new Wildcard($field, $value, $boost);
 
         self::assertEquals([
-            'wildcard' => [ $field => [ 'value' => $value, 'boost' => $boost ] ],
+            'wildcard' => [ $field => [ 'value' => $value, 'boost' => $boost, 'case_insensitive' => false ] ],
         ], $wildcard->build());
     }
 
@@ -30,7 +30,37 @@ class WildcardTest extends TestCase
         $wildcard = new Wildcard($field, $value);
 
         self::assertEquals([
-            'wildcard' => [ $field => [ 'value' => $value, 'boost' => 1.0 ] ],
+            'wildcard' => [ $field => [ 'value' => $value, 'boost' => 1.0, 'case_insensitive' => false ] ],
+        ], $wildcard->build());
+    }
+
+    public function test_it_builds_with_case_insensitive(): void
+    {
+        $field = ':field:';
+        $value = ':value:';
+
+        $wildcard = new Wildcard($field, $value);
+
+        $caseInsensitive = true;
+        $wildcard->setCaseInsensitive($caseInsensitive);
+
+        self::assertEquals([
+            'wildcard' => [ $field => [ 'value' => $value, 'boost' => 1.0, 'case_insensitive' => $caseInsensitive ] ],
+        ], $wildcard->build());
+    }
+
+    public function test_it_builds_with_rewrite(): void
+    {
+        $field = ':field:';
+        $value = ':value:';
+
+        $wildcard = new Wildcard($field, $value);
+
+        $rewrite = 'constant_score';
+        $wildcard->setRewrite($rewrite);
+
+        self::assertEquals([
+            'wildcard' => [ $field => [ 'value' => $value, 'boost' => 1.0, 'case_insensitive' => false, 'rewrite' => $rewrite ] ],
         ], $wildcard->build());
     }
 }
